88a8c8859adde7910d3798fca71f8e025ba61ad8
Dip Chondo Partho <dipparthochondo@gmail.com>
2025-05-06 01:02:28 +0600

    add slider from swiper

--- src/Component/Slider/Slider.jsx
+++ src/Component/Slider/Slider.jsx
@@ -1,44 +1,62 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Slider = () => {
     return (
-        <div className="carousel h-[40rem] rounded-3xl">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img
-                    src="/peakpx.jpg"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide4" className="">❮</a>
-                    <a href="#slide2" className="">❯</a>
-                </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
-                <img
-                    src="/avenger.jpg"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide1" className="">❮</a>
-                    <a href="#slide3" className="">❯</a>
-                </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img
-                    src="/interstellar.jpg"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide2" className="btn-circle text-blue-600">❮</a>
-                    <a href="#slide4" className="btn-circle text-blue-600">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img
-                    src="/superman.jpg"
-                    className="w-full" />
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide3" className="btn-circle">❮</a>
-                    <a href="#slide1" className="">❯</a>
-                </div>
-            </div>
+        // <div className="carousel h-[40rem] rounded-3xl">
+        //     <div id="slide1" className="carousel-item relative w-full">
+        //         <img
+        //             src="/peakpx.jpg"
+        //             className="w-full" />
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        //             <a href="#slide4" className="">❮</a>
+        //             <a href="#slide2" className="">❯</a>
+        //         </div>
+        //     </div>
+        //     <div id="slide2" className="carousel-item relative w-full">
+        //         <img
+        //             src="/avenger.jpg"
+        //             className="w-full" />
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        //             <a href="#slide1" className="">❮</a>
+        //             <a href="#slide3" className="">❯</a>
+        //         </div>
+        //     </div>
+        //     <div id="slide3" className="carousel-item relative w-full">
+        //         <img
+        //             src="/interstellar.jpg"
+        //             className="w-full" />
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        //             <a href="#slide2" className="btn-circle text-blue-600">❮</a>
+        //             <a href="#slide4" className="btn-circle text-blue-600">❯</a>
+        //         </div>
+        //     </div>
+        //     <div id="slide4" className="carousel-item relative w-full">
+        //         <img
+        //             src="/superman.jpg"
+        //             className="w-full" />
+        //         <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        //             <a href="#slide3" className="btn-circle">❮</a>
+        //             <a href="#slide1" className="">❯</a>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className='px-10 rounded-3xl'>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper rounded-3xl h-[60rem]" >
+                <SwiperSlide><img src="/superman.jpg "
+                    className="w-full rounded-3xl" /></SwiperSlide>
+                <SwiperSlide><img src="/interstellar.jpg"
+                    className="w-full rounded-3xl" /></SwiperSlide>
+                <SwiperSlide><img src="/avenger.jpg"
+                    className="w-full rounded-3xl" /></SwiperSlide>
+                <SwiperSlide><img src="/peakpx.jpg"
+                    className="w-full rounded-3xl" /></SwiperSlide>
+            </Swiper>
         </div>
     );
 };
